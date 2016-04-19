dbe970dcb0fbabb9137167a4c536e18d901d80c4
kris <kristenjacobs@outlook.com>
2016-04-19 20:29:16 +0100

    Impemented the bitwise and instruction

--- src/chip8_clj/instructions.clj
+++ src/chip8_clj/instructions.clj
@@ -105,8 +105,16 @@
 
 (defn execute-8XY2
   [machine-state opcode]
-  (log/info "execute-8XY2: Error: Not yet implemented")
-  (System/exit 1))
+  (let [reg-x-num (utils/get-nibble1 opcode)
+        reg-x-val (machine-state/get-register machine-state reg-x-num)
+        reg-y-num (utils/get-nibble2 opcode)
+        reg-y-val (machine-state/get-register machine-state reg-y-num)
+        result (bit-and reg-x-val reg-y-val)]
+    (log/info (format "0x%04x 8XY2 0x%04x and V[%d](0x%02x) = V[%d](0x%02x) & V[%d](0x%02x)" 
+                       (:pc machine-state) opcode reg-x-num result reg-x-num reg-x-val reg-y-num reg-y-val))
+    (-> machine-state
+        (machine-state/set-register reg-x-num result)
+        (machine-state/increment-pc))))
 
 (defn execute-8XY3
   [machine-state opcode]
