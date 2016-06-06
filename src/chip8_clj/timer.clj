5e8e10ca9a8c4586a3918f200af55eb220baf94a
kris <kristen.f.jacobs@oracle.com>
2016-06-06 08:27:56 +0100

    Made the sounds effects work on OSX

--- src/chip8_clj/timer.clj
+++ src/chip8_clj/timer.clj
@@ -3,7 +3,10 @@
 
 (defn play-sound
   []
-  (. (Runtime/getRuntime) exec "paplay resources/sounds/Click1.ogg"))
+  (case (System/getProperty "os.name") 
+    "Linux"    (. (Runtime/getRuntime) exec "paplay resources/sounds/Click1.wav")
+    "Mac OS X" (. (Runtime/getRuntime) exec "afplay resources/sounds/Click1.wav")
+    0))
 
 (defn- tick
   [timer timer-type]
